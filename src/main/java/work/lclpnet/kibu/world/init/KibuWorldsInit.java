8b987262150349cffa7e25b011fd64020c357a2a
LCLP <lukas@lclpnet.work>
2024-04-24 18:26:07 +0200

    introduce fantasy mod dependency

--- src/main/java/work/lclpnet/kibu/world/init/KibuWorldsInit.java
+++ src/main/java/work/lclpnet/kibu/world/init/KibuWorldsInit.java
@@ -1,43 +1,60 @@
 package work.lclpnet.kibu.world.init;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.util.WorldSavePath;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import work.lclpnet.kibu.world.KibuWorlds;
+import work.lclpnet.kibu.world.WorldHandleTracker;
+import work.lclpnet.kibu.world.WorldManager;
+import work.lclpnet.kibu.world.data.LevelDataWriter;
+import work.lclpnet.kibu.world.mixin.MinecraftServerAccessor;
+import work.lclpnet.kibu.world.mixin.fantasy.RuntimeWorldAccessor;
+import xyz.nucleoid.fantasy.RuntimeWorld;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class KibuWorldsInit implements ModInitializer {
 
-    private static final String FANTASY_MOD_ID = "fantasy";
     private static final String KIBU_WORLD_API_MOD_ID = "kibu-world-api";
     public static final Logger LOGGER = LoggerFactory.getLogger(KIBU_WORLD_API_MOD_ID);
-    private static boolean funtional = false;
 
     @Override
     public void onInitialize() {
-        if (!isFantasyPresent()) {
-            LOGGER.warn("Mod '{}' is not loaded. Disabling {}...", FANTASY_MOD_ID, KIBU_WORLD_API_MOD_ID);
-            return;
-        }
+        ServerWorldEvents.UNLOAD.register((server, world) -> {
+            if (!(world instanceof RuntimeWorld runtimeWorld)) return;
 
-        new SafeKibuWorldsInit().initSafe();
+            WorldManager worldManager = KibuWorlds.getInstance().getWorldManager(server);
 
-        funtional = true;
-    }
+            if (worldManager instanceof WorldHandleTracker tracker) {
+                tracker.unregisterWorld(runtimeWorld);
+            }
+        });
 
-    private static boolean isFantasyPresent() {
-        return FabricLoader.getInstance().isModLoaded(FANTASY_MOD_ID);
-    }
+        ServerWorldEvents.LOAD.register((server, world) -> {
+            if (!(world instanceof RuntimeWorld runtimeWorld)) return;
 
-    /**
-     * Check whether kibu-world-api is functional.
-     * The mod might not be functional, if:
-     * <ul>
-     *     <li>it wasn't loaded yet</li>
-     *     <li>dynamic dependencies, such as the 'fantasy' mod are not loaded</li>
-     * </ul>
-     * @return Whether the mod is functional.
-     */
-    public static boolean isFunctional() {
-        return funtional;
+            RuntimeWorld.Style style = ((RuntimeWorldAccessor) runtimeWorld).getStyle();
+
+            if (style != RuntimeWorld.Style.PERSISTENT) return;
+
+            var key = world.getRegistryKey();
+
+            LevelStorage.Session session = ((MinecraftServerAccessor) server).getSession();
+            Path levelDat = session.getWorldDirectory(key).resolve(WorldSavePath.LEVEL_DAT.getRelativePath());
+
+            if (Files.exists(levelDat)) return;
+
+            WorldManager worldManager = KibuWorlds.getInstance().getWorldManager(server);
+
+            if (worldManager instanceof LevelDataWriter writer) {
+                writer.writeLevelData(world);
+            }
+        });
+
+        LOGGER.info("Initialized.");
     }
 }
